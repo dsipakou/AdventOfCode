ddeba0eb2facd1e0b125f14a77679333f8c506c0
flyermc <mc.flyer@gmail.com>
2017-12-05 21:21:07 +0300

    01 part 2 compelted

--- 01.py
+++ 01.py
@@ -1,8 +1,15 @@
+# input_str = str(input())
+# output = 0
+# for i in range(len(input_str) - 1):
+#     if input_str[i] == input_str[i + 1]:
+#         output += int(input_str[i])
+# if input_str[0] == input_str[len(input_str) - 1]:
+#     output += int(input_str[0])
+# print(output)
+
 input_str = str(input())
 output = 0
-for i in range(len(input_str) - 1):
-    if input_str[i] == input_str[i + 1]:
-        output += int(input_str[i])
-if input_str[0] == input_str[len(input_str) - 1]:
-    output += int(input_str[0])
+for i in range(len(input_str) // 2):
+    if input_str[i] == input_str[i + (len(input_str) // 2)]:
+        output += int(input_str[i]) * 2
 print(output)
