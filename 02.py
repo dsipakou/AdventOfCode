438f1fca0247cfdc22e7aaffde330c67e4ec22bb
flyermc <mc.flyer@gmail.com>
2017-12-05 22:48:42 +0300

    Add part 2 for 02

--- 02.py
+++ 02.py
@@ -1,7 +1,30 @@
+# output = 0
+# line = input()
+# while line:
+#     arr = [int(x) for x in line.split()]
+#     output += abs(min(arr) - max(arr))
+#     line = input()
+# print(output)
+
 output = 0
 line = input()
 while line:
+    found = False
     arr = [int(x) for x in line.split()]
-    output += abs(min(arr) - max(arr))
+    for i in range(len(arr) - 1):
+        if found:
+            break
+        for j in range(i + 1, len(arr)):
+            if arr[i] == arr[j]:
+                output += 1
+            elif arr[i] > arr[j]:
+                if arr[i] % arr[j] == 0:
+                    output += arr[i] // arr[j]
+                    found = True
+                    break
+            elif arr[j] % arr[i] == 0:
+                output += arr[j] // arr[i]
+                found = True
+                break
     line = input()
 print(output)
