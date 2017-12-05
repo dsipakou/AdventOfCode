7a140cb2ead674cf24b6e1cacfdd18b2a5f07444
Dzianis Sipakou <d.sipakou@gmail.com>
2017-12-05 17:27:56 +0300

    Add 5th task

---
+++ 05.py
@@ -0,0 +1,17 @@
+line = input()
+arr = []
+while line:
+    arr.append(int(line))
+    line = input()
+
+inside = True
+output = 0
+current_index = 0
+while inside:
+    cur = arr[current_index]
+    arr[current_index] += 1
+    output += 1
+    current_index += cur
+    if current_index < 0 or current_index > len(arr) - 1:
+        inside = False
+print(output)
