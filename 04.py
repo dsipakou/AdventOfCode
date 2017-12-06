7575eabd4cc566fb7350d250d93e1f70014f0262
Dzianis Sipakou <d.sipakou@gmail.com>
2017-12-06 12:14:51 +0300

    Add second part for 4 task

--- 04.py
+++ 04.py
@@ -1,8 +1,20 @@
 from collections import Counter
+# output = 0
+# line = input()
+# while line:
+#     tmp = Counter(line.split(' '))
+#     if sorted(tmp.values(), reverse=True)[0] == 1:
+#         output += 1
+#     line = input()
+# print(output)
+
 output = 0
 line = input()
 while line:
-    tmp = Counter(line.split(' '))
+    arr = line.split(' ')
+    for i in range(len(arr)):
+        arr[i] = ''.join(sorted(arr[i]))
+    tmp = Counter(arr)
     if sorted(tmp.values(), reverse=True)[0] == 1:
         output += 1
     line = input()
