eef8813b0e4f1d21e6ed159904afd7a9d9f5defa
Dzianis Sipakou <d.sipakou@gmail.com>
2017-12-06 12:23:41 +0300

    Add second part for 5 task

--- 05.py
+++ 05.py
@@ -7,9 +7,20 @@ while line:
 inside = True
 output = 0
 current_index = 0
+# while inside:
+#     cur = arr[current_index]
+#     arr[current_index] += 1
+#     output += 1
+#     current_index += cur
+#     if current_index < 0 or current_index > len(arr) - 1:
+#         inside = False
+# print(output)
 while inside:
     cur = arr[current_index]
-    arr[current_index] += 1
+    if cur >= 3:
+        arr[current_index] -= 1
+    else:
+        arr[current_index] += 1
     output += 1
     current_index += cur
     if current_index < 0 or current_index > len(arr) - 1:
