9722a9ecdf70a35dbb9e668030a1761990acbe23
Dzianis Sipakou <d.sipakou@gmail.com>
2017-12-06 12:59:44 +0300

    6 task part one added

---
+++ 06/01.py
@@ -0,0 +1,18 @@
+arr = list(map(int, input().split()))
+dublicate = False
+output = 0
+tmp_arr = []
+while not dublicate:
+    output += 1
+    number = max(arr)
+    index = arr.index(number)
+    arr[index] = 0
+    for i in range(number):
+        arr[(index + i + 1) % len(arr)] += 1
+    if arr not in tmp_arr:
+        tmp_arr.append([x for x in arr])
+    else:
+        dublicate = True
+    print(arr)
+
+print(output)
