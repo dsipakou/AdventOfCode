62ec3ea5abef0421849e21e42a0a3b764c37c7c7
Dzianis Sipakou <d.sipakou@gmail.com>
2017-12-11 12:07:44 +0300

    Day 11 part 1 add

--- 11/01.py
+++ 11/01.py
@@ -2,3 +2,19 @@ from collections import Counter
 s = input().split(',')
 
 c = Counter(s)
+print(c)
+min1 = min(c['s'], c['n'])
+c['s'] -= min1
+c['n'] -= min1
+min2 = min(c['nw'], c['se'])
+c['nw'] -= min2
+c['se'] -= min2
+min3 = min(c['sw'], c['ne'])
+c['sw'] -= min3
+c['ne'] -= min3
+print(c)
+min4 = min(c['s'], c['ne'])
+c['se'] += min4
+c['s'] -= min4
+c['ne'] -= min4
+print(sum(c.values()))
